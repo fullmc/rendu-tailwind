d028d03722b08a3a22bb8dc9b87664be4fedd220
fullmc <lambomarieclaire@yahoo.fr>
2022-11-18 11:07:41 +0100

    1er commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage : {
+        'img_header' : "url('/images/mobile/image-header.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
