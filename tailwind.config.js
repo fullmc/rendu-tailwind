48e5fdee88200f13f3ebe79df61bb0ac52a636df
fullmc <lambomarieclaire@yahoo.fr>
2022-11-18 13:29:24 +0100

    ajout section 'transform your brand' + 'stand out the right audience' :sparkles:

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
         'Barlow' : ['Barlow', 'sans-serif'],
         'Fraunces' : ['Fraunces', 'serif'],
       },
+      colors : {
+        'gris-p': 'hsl(232, 10%, 55%)',
+        'titres' : 'hsl(212, 27%, 19%)',
+        'barre-more' : 'hsla(51, 100%, 49%, 40%)',
+        'barre-more-pink' : 'hsla(7, 99%, 70%, 40%)'
+      }
     },
   },
   plugins: [],
