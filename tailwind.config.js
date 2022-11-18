11ac7e4c3338f36d756d054f8310e74b6473d42d
fullmc <lambomarieclaire@yahoo.fr>
2022-11-18 16:01:05 +0100

    ajout section clients + footer :sparkles:

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       backgroundImage : {
         'img_header' : "url('/images/mobile/image-header.jpg')",
         'cerises' : "url('/images/mobile/image-graphic-design.jpg')",
-        'clementines' : "url('/images/mobile/image-photography.jpg')",   
+        'clementines' : "url('/images/mobile/image-photography.jpg')", 
       },
       fontFamily: {
         'Barlow' : ['Barlow', 'sans-serif'],
@@ -18,8 +18,15 @@ module.exports = {
         'barre-more' : 'hsla(51, 100%, 49%, 40%)',
         'barre-more-pink' : 'hsla(7, 99%, 70%, 40%)',
         'cerises' : 'hsl(167, 40%, 24%)',
-        'clementine' : 'hsl(198, 62%, 26%)'
-      }
+        'clementine' : 'hsl(198, 62%, 26%)',
+        'grey-client' : 'hsl(210, 4%, 67%)',
+        'client-p' : 'hsl(212, 27%, 19%)',
+        'footer' : 'rgb(159, 210, 197)',
+        'svg' : 'rgb(68,119,105)',
+      },
+      letterSpacing: {
+        'more-wide':'.4em',
+      },
     },
   },
   plugins: [],
